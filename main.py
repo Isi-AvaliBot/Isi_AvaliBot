f1840d27387966c4c4f7f7929102ca4fd17bad67
isi_01 <maksimsur777@gmail.com>
2021-08-25 21:29:02 +0300

    v0.4

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ import wiki
 import pain
 
 bot = commands.Bot(command_prefix = settings['prefix'])
- 
+bot.remove_command('help')
+
+
+@bot.command()
+async def help(ctx):
+    await ctx.send(embed= discord.Embed(description = '**enc <text>** - this function allows you to translate English into aval\n**dec <text>** - this function allows you to translate avalyn into english\n**awiki <text>** - this function allows you to find information by keyword or phrase\n**avali** - get a random picture of avali from the 1,500 library\n**team** - here you can find information about everyone who took part in the development of the bot'))
 @bot.command()
 async def team(ctx):
     await ctx.send(embed= discord.Embed(title="coconut team", description = "<@385504411763605505> - discord bot | report an issue\n<@528586514213371915> - $encoder-decoder\n<@290350813828874241> - $avali"))
@@ -25,18 +30,16 @@ async def dec(ctx):
     await ctx.send(dec)
 @bot.command()
 async def coconut(ctx):
- await ctx.send(
-    'кокоскокоскокcoconutкокосcoconutcockкокосcoconutcoconutкокос https://cdn.discordapp.com/attachments/845189247824822315/876813997905686558/cock.jpg'
-    )
+ await ctx.send(embed= discord.Embed(title=
+    'кокос:coconut:кокоскокcoconut:coconut:кокосcoconutcockкокосcoconutcoconutкокос:coconut:'))
 @bot.command()
 async def awiki(ctx):
     wik = wiki.serch(ctx.message.content.replace('$wiki ', ''))
-    await ctx.send(wik) 
+    await ctx.send(embed= discord.Embed(title='maybe the answer is here ', description = wik) )
 
 @bot.command()
 async def avali(ctx):
     avalik = pain.ava()
     await ctx.send(avalik) 
- 
 
-bot.run(settings['token'])
+bot.run('token')
