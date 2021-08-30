aafd1959054f058f748a2e31016ac219152290e1
isi_01 <maksimsur777@gmail.com>
2021-08-30 16:58:53 +0300

    v0.4-1520art

--- pain.py
+++ pain.py
@@ -2,6 +2,6 @@ import requests
 import random
 
 def ava():
- oval = requests.get('https://api.vk.com/method/wall.search?owner_id=-186164216&query=%23avali&count=1&offset='+str(random.randint(0, 1494))+'&access_token=6666aad56666aad56666aad5c7661fa343666666666aad50747f80b4d281fa82d834e5a&v=5.123').json()
+ oval = requests.get('https://api.vk.com/method/wall.search?owner_id=-186164216&query=%23avali&count=1&offset='+str(random.randint(0, 1520))+'&access_token=6666aad56666aad56666aad5c7661fa343666666666aad50747f80b4d281fa82d834e5a&v=5.123').json()
  oval2 = oval.get('response').get('items')[0].get('attachments')[0].get('photo').get('sizes')
  return oval2[len(oval2)-1].get('url')
