829e15352d8fa1f901ca6aeed21bbd4ae150c104
isi_01 <maksimsur777@gmail.com>
2021-09-04 14:41:55 +0300

    0.5 v4.1

--- main.py
+++ main.py
@@ -57,11 +57,11 @@ async def isitell(ctx):
 @commands.cooldown(rate=1, per=4, type=commands.BucketType.user)
 @bot.command()
 async def awikihelp(ctx):
-    embed1 = discord.Embed(title='Страница 1', discription='описание 2')
-    embed2 = discord.Embed(title='Страница 2', discription='описание 2')
-    embed3 = discord.Embed(title='Страница 3', discription='описание 3')
+    embed1 = discord.Embed(title='Страница 1', description='описание 2')
+    embed2 = discord.Embed(title='Страница 2', description='описание 2')
+    embed3 = discord.Embed(title='Страница 3', description='описание 3')
     em = [embed1, embed2, embed3]
     message = await ctx.send(embed=embed1)
-    pag = page(bot, message, only=ctx.author, use_more=False, embeds=em)
+    pag = page(bot, message, embeds=em)
     await pag.start()
 bot.run('ODc2NTE1MDE2MTQzMTQ3MTEw.YRlMOA.q7Cq8x9ncI2tWrjVhyPxFK57RqM')
