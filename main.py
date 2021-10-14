c770b29f4ba2a7423b2939864842477034d2af1e
isi_01 <maksimsur777@gmail.com>
2021-10-14 22:33:50 +0300

    0.7.3 awiki+stbAwiki

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from config import settings
 import json
 import requests
 
+import twiki
 import enc_dec
 import wiki
-
+from scrape import scraper
 from casher import cash
 import threading
 cash = cash()
@@ -34,11 +35,11 @@ async def help(ctx):
     for s in activeServers:
       summ += len(s.members)
     summ = str(summ)
-    await ctx.send(embed= discord.Embed(description = 'use $ prefix before commands\n**enc <text>** - this function allows you to translate English into aval\n**dec <text>** - this function allows you to translate avalyn into english\n**awiki <text>** - this function allows you to find information by keyword or phrase\n**awikihelp** - list of requests for **awiki**\n**avali** - get a random picture of avali from the 1,500 library\n**team** - here you can find information about everyone who took part in the development of the bot\n**invite** - link to add a bot\n\n\n now the bot is on '+ summ +' servers\nDiscord server\nhttps://discord.gg/43NJF983jZ'))
+    await ctx.send(embed= discord.Embed(description = 'use $ prefix before commands\n**enc <text>** - this function allows you to translate English into avali\n**dec <text>** - this function allows you to translate avalyn into english\n**awiki <text>** - allows you to search for information about avali on wikipedia\n**stbAwiki** - With this command you can access crafts, object appearances and quick references to the official wiki for a number of topics. \n**avali** - get a random picture of avali from the 1,500 library\n**team** - here you can find information about everyone who took part in the development of the bot\n**invite** - link to add a bot\n\n\n now the bot is on '+ summ +' servers\nDiscord server\nhttps://discord.gg/43NJF983jZ'))
 @commands.cooldown(rate=1, per=7, type=commands.BucketType.user)
 @bot.command()
 async def team(ctx):
-    await ctx.send(embed= discord.Embed(title="coconut team", description = "<@385504411763605505> - discord bot | report an issue\n<@528586514213371915> - $encoder-decoder\n<@290350813828874241> - $avali"))
+    await ctx.send(embed= discord.Embed(title="coconut team", description = "<@385504411763605505> - discord bot | report an issue\n<@528586514213371915> - $encoder-decoder\n<@290350813828874241> - $avali\n<@351404235578933249> - new $awiki, $stbAwiki, cash system for $avali"))
 @commands.cooldown(rate=1, per=3, type=commands.BucketType.user)    
 @bot.command()   
 async def enc(ctx):
@@ -54,19 +55,52 @@ async def dec(ctx):
 async def invite(ctx):
   await ctx.send(embed= discord.Embed(title='https://discord.com/api/oauth2/authorize?client_id=876515016143147110&permissions=534723820608&scope=bot'))
 
+@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)  
+@bot.command()
+async def stbAwiki(ctx):
+  s = scraper()
+  msg = ctx.message.content.split(' ')[1:]
+  print(msg)
+  if len(msg) == 0:
+    s = s.map(loc='https://avali.fandom.com/wiki/Items')
+    await ctx.send(embed = s)
+  if len(msg) == 1:
+    context = ' '.join(msg)
+    e = s.map(loc='https://avali.fandom.com/wiki/Items',context=context)
+    try:
+      await ctx.send(embed = e)
+    except:
+      await ctx.send("Failed, did you specify in which category I should look? <:blep00:897749326745456651>")
+  else:
+    context = ' '.join(msg)
+    s.load(loc='https://avali.fandom.com/wiki/Items',context=context)
+    craft = s.get_craft(s.out)
+    await ctx.send(embed = craft)
 
+@bot.command()
+async def awiki(ctx):
+  msg = ctx.message.content.split(' ')
+  loc=''
+  msg = msg[1:]
+  omsg=msg
+  if 'in' in msg:
+    loc = msg[1]
+    omsg = msg[2:]
+  data = twiki.scrape(loc=loc)
+  engine = twiki.engine(data)
+  msg = engine.load(' '.join(omsg))
+  embed=discord.Embed()
+  embed.set_thumbnail(url=twiki.image(loc=loc))
+  embed.add_field(name=str(' '.join(omsg)).upper(), value=msg, inline=False)
+  await ctx.send(embed=embed)
 @commands.cooldown(rate=1, per=120, type=commands.BucketType.user)    
 @bot.command()
 async def coconut(ctx):
  await ctx.send(embed= discord.Embed(title=
-    'кокос:coconut:кокоскокcoconut:coconut:кокосcoconutcockкокосcoconutcoconutкокос:coconut: 0.7.2\n - add cash sistem'))
+    'кокос:coconut:кокоскокcoconut:coconut:кокосcoconutcockкокосcoconutcoconutкокос:coconut: 0.7.3\n - new wiki'))
 @commands.cooldown(rate=1, per=3, type=commands.BucketType.user)    
 @bot.command()
-async def awiki(ctx):
-    wik = wiki.serch(ctx.message.content)
-    await ctx.send(embed= discord.Embed(title='maybe the answer is here ', description = wik) )
-@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
-@bot.command()
+
 async def avali(ctx):
     url = cash.request()
     embed=discord.Embed(title='Art by'+url.author)
@@ -74,17 +108,6 @@ async def avali(ctx):
     #imag = pain.ava()
     await ctx.send(embed=embed)
 
-
-@commands.cooldown(rate=1, per=20, type=commands.BucketType.user)
-@bot.command()    
-async def awikihelp(ctx):
-    embed1 = discord.Embed(title='page 1', description='A\nammonia\narmor\naugmentation\nC\nculture\nD\ndifferences\ndomestic technology\ndrone\nF\nfactions\nflight\nG\ngovernance\ngrowth')
-    embed2 = discord.Embed(title='page 2', description='H\nheight\nhousehold\nhow many live\nI\nIlluminate\nindependent worlds\nK\nknown worlds\nL\nlength\nM\nmedicine\nN\nnexus')
-    embed3 = discord.Embed(title='page 3', description='P\npack\nR\nreproduction\nS\nscientific\nsize\nspace\nspace discovery\nsurvival\nT\ntechnology\ntribe\nW\nweaponry\nwings')
-    em = [embed1, embed2, embed3]
-    message = await ctx.send(embed=embed1)
-    pag = page(bot, message, only=ctx.author, use_more=False, embeds=em)
-    await pag.start()
 
 
 
---
+++ twiki.py
@@ -0,0 +1,89 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import difflib
+from random import choice
+
+def scrape(loc=''):
+  "Scrape first"
+  x = requests.get('https://avali.fandom.com/wiki/'+loc)
+  s = bs(x.content, 'html.parser')
+  test = s.find(id='mw-content-text').find_all("p")
+  y = ''
+  for x in test:
+    y+='\n'+x.text
+  return y
+
+def image(loc=''):
+  x = requests.get('https://avali.fandom.com/wiki/'+loc)
+  s = bs(x.content, 'html.parser')
+  links = []
+  for link in s.find(id='mw-content-text').select("img[src^=http]"):
+    lnk = link.get('src').split('/revision/')[0]
+    links.append(lnk)
+  return choice(links)
+  
+
+"Asking the bot; Who is avali?"
+# Define out engine
+class engine:
+  def __init__(self,data):
+    self.cur_line = ''
+    self.keys = []
+    self.location = None
+    self.out = ''
+    self.text = ''
+    self.json = {}
+    self.data = data.split('\n')
+  
+  def load(self,text,page=[]):
+    self.page = page
+    self.text = text
+    self.keys = text.split(' ')
+    self.proc()
+    return self.get()
+
+  def proc(self):
+    json = {
+      'keys':{
+      },
+    }
+    for line in self.data:
+      print('+++',line)
+      for word in line.split(' '):
+        print('---',word)
+        if word in json['keys']:
+          json['keys'][word]['locations'].append(self.data.index(line))
+          json['keys'][word]['pages'].append(self.page)
+        else:
+          json['keys'][word] = {}
+          json['keys'][word]['locations'] = [self.data.index(line)]
+          json['keys'][word]['pages'] = [self.page]
+    del json['keys']['']
+    self.json = json
+
+  def get(self):
+    print(self.json)
+    for key in self.text.split(' '):
+      if key in self.json['keys']:
+        print('===',self.json['keys'][key])
+        return self.data[self.json['keys'][key]['locations'][0]]
+    return 'Nothing found ¯\_(ツ)_/¯'
+
+def remove_empty_from_dict(d):
+    if type(d) is dict:
+        return dict((k, remove_empty_from_dict(v)) for k, v in d.items() if v and remove_empty_from_dict(v))
+    elif type(d) is list:
+        return [remove_empty_from_dict(v) for v in d if v and remove_empty_from_dict(v)]
+    else:
+        return d
+
+data = scrape()
+# Input
+inp = 'Starbound'
+# Create an engine instance
+e = engine(data)
+# Load data into engine
+print(e.data)
+match = e.load(inp)
+print(match)
+
