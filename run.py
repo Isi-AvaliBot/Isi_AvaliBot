497ca7fad8e00591e1c210b320c52ba3b151e2b1
isi_01 <maksimsur777@gmail.com>
2021-10-20 16:35:06 +0300

    0.8 website

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from app import create_app
+import os
 from threading import Thread
 
 application = create_app('config.BaseConfig')
 
 def run():
-  application.run(port=5000)
+  application.run(port=int(os.environ.get('PORT', 33507)))
 
 def keep_alive():
     server = Thread(target=run)
