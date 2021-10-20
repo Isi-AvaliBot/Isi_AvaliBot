9735d1d2203c8ecd1a80ebe0228f05514726eeea
isi_01 <maksimsur777@gmail.com>
2021-10-20 16:16:53 +0300

    0.8 website

--- main.py
+++ main.py
@@ -166,4 +166,4 @@ async def on_ready():
   print('Bot is live!')
 
 keep_alive()
-bot.run  ('ODgwMDU1MzE5MDMwNTM4MjUw.YSYtYg.o-NkQ_GlAeyCP5CqBLEiy1pwQWY')
+bot.run  ('ODc2NTE1MDE2MTQzMTQ3MTEw.YRlMOA.q7Cq8x9ncI2tWrjVhyPxFK57RqM')
